30f8f53afef7476bb41baab353dcdf9676098d04
Victor Otávio Ferreira <victor@ferreira.id>
2024-10-21 09:28:33 -0300

    Refactor code to handle HTTP requests and serve static files

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,31 @@
-console.log("Hello, world!");
+import { readFile } from "fs/promises";
+import http, { IncomingMessage, ServerResponse } from "http";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const host = "0.0.0.0";
+const port = 8080;
+
+const httpServer = http.createServer(httpHandler);
+
+httpServer.listen(port, host, () => {
+  console.log(`HTTP server running at http://${host}:${port}/`);
+});
+
+async function httpHandler(req: IncomingMessage, res: ServerResponse) {
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const publicFile = path.join(__dirname, "../public", req.url || "");
+
+  try {
+    const data = await readFile(publicFile);
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write(data);
+  } catch (err) {
+    console.error(err);
+    res.writeHead(404, { "Content-Type": "text/plain" });
+    res.write("404 Not Found");
+  } finally {
+    res.end();
+  }
+}
